c177c35968db295b08cae6ccd51448257bfdf951
Timon de Morel <tdemorel@cardiweb.com>
2020-05-17 15:54:30 +0200

    Mise à jour boilerplate : ajout d'un mouse listener, d'une fonction de couleur random, d'une fonction init.

--- canvas_boilerplate/canvas.js
+++ canvas_boilerplate/canvas.js
@@ -1,18 +1,42 @@
+//UTILS
+
 const randomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const canvas = document.querySelector("canvas")
+const colors = ["#6C7780", "#8CC9FF", "#DAEEFF", "#466580", "#ADBECC"]
+const randomColor = () => {
+  const index = randomInt(0, 4)
+  return colors[index]
+}
 
+//CANVAS
+const canvas = document.querySelector("canvas")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-
 const c = canvas.getContext("2d")
 
+//WINDOW
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
 })
+
+//MOUSE
+let mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+}
+window.addEventListener("mousemove", (event) => {
+  mouse.x = event.clientX
+  mouse.y = event.clientY
+})
+
+//IMPLEMENTATION
+
+const init = () => {}
+
+//ANIMATION
 
 const animate = () => {
   requestAnimationFrame(animate)
