b5424e1d90c3e0f3f9761393ce2605845cfb70f7
Timon de Morel <tdemorel@cardiweb.com>
2020-06-12 22:42:37 +0200

    Adding sparks to flame. Be careful not to burn your house down..

--- my_canvas/smoke/canvas.js
+++ my_canvas/smoke/canvas.js
@@ -10,6 +10,10 @@ const randomColor = (colorArray) => {
   return colorArray[index]
 }
 
+const randomDecim = (min, max) => {
+  return Math.random() * (max - min) + min
+}
+
 const getDistance = (x1, y1, x2, y2) => {
   const xDistance = x2 - x1
   const yDistance = y2 - y1
@@ -54,7 +58,7 @@ function Flame(x, y, radius, color) {
   this.radius = radius
   this.color = color
   this.opacity = INIT_OPACITY
-  this.sparks = generateSparks(this.x, this.y, this.color, 3)
+  this.sparks = [] // generateSparks(this.x, this.y, this.color, 2)
 
   this.update = () => {
     this.x += this.dx
@@ -63,6 +67,7 @@ function Flame(x, y, radius, color) {
     this.opacity += -0.02
     this.opacity = Math.max(this.opacity, 0)
     this.radius = Math.max(this.radius - 1, 0)
+    this.sparks.forEach((s) => s.update())
     this.draw()
   }
   this.draw = () => {
@@ -84,19 +89,17 @@ function Flame(x, y, radius, color) {
 function Spark(x, y, radius, color) {
   this.x = x
   this.y = y
-  this.dx = -2
-  this.dy = -10
-  this.radian = 0
+  this.dx = 0
+  this.dy = -randomInt(3, 7)
+  this.radian = randomDecim(0, Math.PI * 2)
   this.radius = radius
-  this.color = "red"
+  this.color = "orange"
 
   this.update = () => {
     this.x += this.dx
     this.y += this.dy
-    this.radian += Math.PI / 6
-    this.dx += this.dx + Math.cos(this.radian)
-    this.dy += this.dy + Math.sin(this.radian)
-    this.sparks.forEach((s) => s.update())
+    this.radian += randomInt(Math.PI / 50, Math.PI / 30)
+    this.dx = randomInt(1, 3) * Math.cos(this.radian)
     this.draw()
   }
 
@@ -112,9 +115,9 @@ function Spark(x, y, radius, color) {
 const generateSparks = (x, y, color, qty) => {
   let sparks = []
   for (let i = 0; i < qty; i++) {
-    const sX = randomInt(x - 5, x + 5)
-    const sY = randomInt(y - 5, y + 5)
-    sparks.push(new Spark(sX, sY, 3, color))
+    const sX = randomInt(x - 10, x + 10)
+    const sY = randomInt(y - 10, y + 10)
+    sparks.push(new Spark(sX, sY, 1, color))
   }
   return sparks
 }
@@ -144,8 +147,19 @@ const animate = () => {
       flame.y = randomInt(mouse.y - 5, mouse.y + 5)
       flame.opacity = INIT_OPACITY
       flame.radius = randomInt(15, 40)
-      flame.sparks = generateSparks(flame.x, flame.y, flame.color, 3)
+      //flame.sparks = generateSparks(flame.x, flame.y, flame.color, 2)
     }
+    if (Math.random() > 0.99 && flame.sparks.length < 2) {
+      const sX = randomInt(mouse.x - 10, mouse.x + 10)
+      const sY = randomInt(mouse.y - 10, mouse.y + 10)
+      flame.sparks.push(new Spark(sX, sY, 1, flame.color))
+    }
+    flame.sparks.forEach((spark) => {
+      if (spark.y <= 0) {
+        spark.y = randomInt(mouse.y - 10, mouse.y + 10)
+        spark.x = randomInt(mouse.x - 10, mouse.x + 10)
+      }
+    })
   })
 }
 
