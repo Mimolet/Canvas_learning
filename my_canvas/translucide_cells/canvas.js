5ee616c9d4f012b22847065560fd5f461893a3f7
Timon de Morel <tdemorel@cardiweb.com>
2020-06-12 20:28:03 +0200

    Création de cellules qui se colorent avec un linera gradient à proximité de la souris

--- my_canvas/translucide_cells/canvas.js
+++ my_canvas/translucide_cells/canvas.js
@@ -37,22 +37,36 @@ window.addEventListener("mousemove", (event) => {
   mouse.x = event.clientX
   mouse.y = event.clientY
 })
+window.addEventListener("click", (event) => {
+  init()
+})
+
+const MOUSE_VISCINITY = 200
 
 //OBJECT
 
-function Object(x, y, radius, color) {
+function Cell(x, y, radius, color) {
   this.x = x
   this.y = y
   this.radius = radius
   this.color = color
+  this.isNearMouse = false
 
   this.update = () => {
+    if (getDistance(this.x, this.y, mouse.x, mouse.y) < MOUSE_VISCINITY) {
+      this.isNearMouse = true
+    } else {
+      this.isNearMouse = false
+    }
     this.draw()
   }
   this.draw = () => {
+    const grad = c.createLinearGradient(mouse.x, mouse.y, this.x, this.y)
+    grad.addColorStop(0, "green")
+    grad.addColorStop(1, "black")
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
-    c.fillStyle = this.color
+    c.fillStyle = this.isNearMouse ? grad : this.color
     c.fill()
     c.closePath()
   }
@@ -60,7 +74,35 @@ function Object(x, y, radius, color) {
 
 //IMPLEMENTATION
 
-const init = () => {}
+const generateCells = (nbCells) => {
+  let res = []
+  for (let i = 0; i < nbCells; i++) {
+    const radius = randomInt(30, 50)
+    let x = randomInt(0, innerWidth)
+    let y = randomInt(0, innerHeight)
+
+    // on ne veut pas que les particules overlap
+    if (i !== 0) {
+      for (let j = 0; j < res.length; j++) {
+        if (getDistance(x, y, res[j].x, res[j].y) - radius - res[j].radius < 0) {
+          x = randomInt(radius, innerWidth - radius)
+          y = randomInt(radius, innerHeight - radius)
+
+          // sera incrémenté à 0 à la fin de l'itération
+          j = -1
+        }
+      }
+    }
+
+    res.push(new Cell(x, y, radius, "black"))
+  }
+  return res
+}
+
+let cells = []
+const init = () => {
+  cells = generateCells(80)
+}
 
 //ANIMATION
 
@@ -68,6 +110,7 @@ const animate = () => {
   requestAnimationFrame(animate)
 
   c.clearRect(0, 0, innerWidth, innerHeight)
+  cells.forEach((cell) => cell.update())
 }
 
 init()
